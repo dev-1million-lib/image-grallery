d8be010e89b5b329a38a1cc4995a57ee28aa74f2
Giu Bin Kang <gbk@1milliondance.com>
2019-08-12 10:39:32 +0900

    초기 커밋

---
+++ js/image-gallery.js
@@ -0,0 +1,21 @@
+var image_gallery = (function () {
+    
+    var init = function() {
+        
+    }
+    
+    var gallery_popup= function () {
+        // $('#artist-image-gallery').on('click', function() {
+        //     $('#popup').addClass('popup-visibility');
+        //   });
+          
+        //   $('.popup-close-btn').on('click', function() {
+        //     $('#popup').removeClass('popup-visibility');
+        //   });
+        //   console.log(1);
+    }
+
+    return{
+        init : init
+    }
+})();
